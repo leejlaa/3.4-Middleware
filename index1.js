eed8daee27adf6ec636afed6a07f27ea773ae195
leejlaa <lejladoric00@gmail.com>
2023-09-15 18:12:41 +0200

    message

---
+++ index1.js
@@ -0,0 +1,32 @@
+import express from "express";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import bodyParser from "body-parser";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const app = express();
+const port = 3000;
+
+var text = app.use(bodyParser.urlencoded({extended: true}));
+
+app.post("/submit",(req,res)=>{
+  res.send(req.body.street+" "+req.body.pet);
+});
+
+
+
+
+
+
+
+
+
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
